e86b83bbb5e4961f4a9cbf1b71626e3c25967199
Semyen747 <combatpilot45@gmail.com>
2023-01-11 04:48:30 +0300

    rebuild repo

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -4,15 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class StatisticsServiceTest {@Test
-void findMax() {
-    StatisticsService service = new StatisticsService();
+public class StatisticsServiceTest {
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
 
-    long actual = service.findMax(incomesInBillions);
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-    assertEquals(expected, actual);
-}
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
